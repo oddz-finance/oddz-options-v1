2bea9730ce9fc800325264b6ecdbd20e4dbc7e17
Krupa Sagar <sagar.krupahb2000@gmail.com>
2021-01-14 10:05:45 +0530

    refactor: move interface to respective directory

--- contracts/Oracle/IOddzOracle.sol
+++ contracts/Oracle/IOddzOracle.sol
@@ -1,16 +1,15 @@
-pragma solidity =0.7.6;
-
-import '../Util/Ownable.sol';
+// SPDX-License-Identifier: BSD-4-Clause
+pragma solidity ^0.7.0;
 
 interface IOddzOracle {
     /**
-      * @dev Function to get the price for an asset 
+      * @dev Function to get the price for an asset
       * @param asset Asset
       * @return uint256 the price in 1e8
     */
     function getPrice(address asset) external view returns (uint256);
     /**
-      * @dev Function to get the price for an underlying asset 
+      * @dev Function to get the price for an underlying asset
       * @param cToken Underlying Asset
       * @return uint256 the price in 1e8
     */
