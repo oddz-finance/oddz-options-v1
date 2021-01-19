0a5ee63341428701581a1e428bc730fd19955e01
Krupa Sagar <sagar.krupahb2000@gmail.com>
2021-01-19 12:35:49 +0530

    feat: update liquidity pool interface

--- contracts/Option/ILiquidityPool.sol
+++ contracts/Option/ILiquidityPool.sol
@@ -5,7 +5,16 @@ pragma solidity ^0.7.0;
 interface ILiquidityPool {
     struct LockedLiquidity { uint amount; uint premium; bool locked; }
 
-    function unlock(uint256 id) external;
-    function send(uint256 id, address payable account, uint256 amount) external;
+    event Profit(uint indexed _id, uint _amount);
+    event Loss(uint indexed _id, uint _amount);
+    event Provide(address indexed _account, uint256 _amount, uint256 _writeAmount);
+    event Withdraw(address indexed _account, uint256 _amount, uint256 _writeAmount);
+
+    function provide() external payable returns (uint256 mint);
+    function withdraw(uint256 _amount) external returns (uint256 burn);
+    function lock(uint256 _id, uint256 _amount) external;
+    function unlock(uint256 _id) external;
+    function send(uint256 _id, address payable _account, uint256 _amount) external;
+    function availableBalance() external view returns (uint256 amount);
     function totalBalance() external view returns (uint256 amount);
 }
