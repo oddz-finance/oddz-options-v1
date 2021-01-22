a65673cf40b972cb05d548d45cded07285ab775b
Krupa Sagar <sagar.krupahb2000@gmail.com>
2021-01-22 21:20:27 +0530

    test: asset add test cases

--- test/behaviors/OddzOptionManager.behavior.ts
+++ test/behaviors/OddzOptionManager.behavior.ts
@@ -15,6 +15,37 @@ export function shouldBehaveLikeOddzOptionManager(): void {
       )
     ).to.be.revertedWith("Invalid Asset");
   });
+  it("should fail with message invalid asset", async function () {
+    const oddzOptionManager = await this.oddzOptionManager.connect(this.signers.admin);
+    // call should be optionType.call
+    const assetId = await oddzOptionManager.addAsset(
+      utils.formatBytes32String("ETH"),
+      BigNumber.from(100000),
+    );
+    await expect(oddzOptionManager.addAsset(
+      utils.formatBytes32String("ETH"),
+      BigNumber.from(100000),
+    )).to.be.revertedWith("Asset already present");
+  });
+  it("should return newly added asset id", async function () {
+    const oddzOptionManager = await this.oddzOptionManager.connect(this.signers.admin);
+    const assetId = await oddzOptionManager.addAsset(
+      utils.formatBytes32String("ETH"),
+      BigNumber.from(100000),
+    );
+    expect(assetId.value.toNumber()).to.equal(0);
+  });
+  // it("should return newly added asset ids for multiple assets", async function () {
+  //   const oddzOptionManager = await this.oddzOptionManager.connect(this.signers.admin);
+  //   await expect(oddzOptionManager.addAsset(
+  //     utils.formatBytes32String("ETH"),
+  //     BigNumber.from(100000),
+  //   )).to.be.equal(0);
+  //   await expect(oddzOptionManager.addAsset(
+  //     utils.formatBytes32String("BTC"),
+  //     BigNumber.from(100000),
+  //   )).to.be.equal(1);
+  // });
   it("should return the premium price", async function () {
 
     const oddzOptionManager = await this.oddzOptionManager.connect(this.signers.admin);
