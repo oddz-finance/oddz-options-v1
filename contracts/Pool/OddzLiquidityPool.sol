35dcd92ba306e975f5e73522c47bd159c97bad95
Krupa Sagar <sagar.krupahb2000@gmail.com>
2021-01-31 20:17:26 +0530

    feat: fix remove liquidity

--- contracts/Pool/OddzLiquidityPool.sol
+++ contracts/Pool/OddzLiquidityPool.sol
@@ -71,41 +71,62 @@ contract OddzLiquidityPool is Ownable, IOddzLiquidityPool, ERC20("Oddz USD LP to
      */
     LockedLiquidity[] public lockedLiquidity;
 
-    function provide() external payable override returns (uint256 mint) {
-        addLiquidity();
+    function addLiquidity() external payable override returns (uint256 mint) {
+        addLiquiditySub();
         uint256 supply = totalSupply();
         uint256 balance = totalBalance();
         if (supply > 0 && balance > 0) mint = msg.value.mul(supply).div(balance.sub(msg.value));
         else mint = msg.value.mul(INITIAL_RATE);
 
-        require(mint > 0, "Pool: Amount is too small");
+        require(mint > 0, "LP: Amount is too small");
 
         _mint(msg.sender, mint);
-        emit Provide(msg.sender, msg.value, mint);
+        emit AddLiquidity(msg.sender, msg.value, mint);
     }
 
-    function withdraw(uint256 _amount) external override returns (uint256 burn) {
+    function removeLiquidity(uint256 _amount) external override returns (uint256 burn) {
         require(
             _amount <= availableBalance(),
-            "Pool Error: Not enough funds on the pool contract. Please lower the amount."
+            "LP Error: Not enough funds on the pool contract. Please lower the amount."
         );
 
         daysActiveLiquidity[getPresentDayTimestamp()].sub(_amount);
-        burn = divCeil(_amount.mul(totalSupply()), totalBalance());
+        burn = divisionCeiling(_amount.mul(totalSupply()), totalBalance());
 
-        require(burn <= balanceOf(msg.sender), "Pool: Amount is too large");
-        require(burn > 0, "Pool: Amount is too small");
+        require(burn <= balanceOf(msg.sender), "LP: Amount is too large");
+        require(burn > 0, "LP: Amount is too small");
+
+        removeLiquidtySub(_amount);
 
         _burn(msg.sender, burn);
-        emit Withdraw(msg.sender, _amount, burn);
+        emit RemoveLiquidity(msg.sender, _amount, burn);
         msg.sender.transfer(_amount);
     }
 
-    function lock(uint256 _id, uint256 _amount) external payable override onlyOwner {
-        require(_id == lockedLiquidity.length, "Wrong id");
+    function removeLiquidtySub(uint256 _amount) private {
+        uint256 lid = 0;
+        while (_amount > 0 && lid < providerLiquidityList[msg.sender].length) {
+            Liquidity storage liquidity = providerLiquidityList[msg.sender][lid];
+            uint256 remAmount = liquidity.amount.sub(liquidity.withdrawn);
+            if (remAmount > 0) {
+                if (remAmount > _amount) {
+                    liquidity.withdrawn = liquidity.withdrawn.add(_amount);
+                    _amount = 0;
+                    break;
+                } else {
+                    liquidity.withdrawn = liquidity.withdrawn.add(remAmount);
+                    _amount = _amount.sub(remAmount);
+                    lid++;
+                }
+            }
+        }
+    }
+
+    function lockLiquidity(uint256 _id, uint256 _amount) external payable override onlyOwner {
+        require(_id == lockedLiquidity.length, "LP: Invalid id");
         require(
             lockedAmount.add(_amount).mul(10) <= totalBalance().sub(msg.value).mul(reqBalance),
-            "Pool Error: Amount is too large."
+            "LP Error: Amount is too large."
         );
 
         lockedLiquidity.push(LockedLiquidity(_amount, msg.value, true));
@@ -113,9 +134,9 @@ contract OddzLiquidityPool is Ownable, IOddzLiquidityPool, ERC20("Oddz USD LP to
         lockedAmount = lockedAmount.add(_amount);
     }
 
-    function unlock(uint256 _id) external override onlyOwner {
+    function unlockLiquidity(uint256 _id) external override onlyOwner {
         LockedLiquidity storage ll = lockedLiquidity[_id];
-        require(ll.locked, "LockedLiquidity with such id has already unlocked");
+        require(ll.locked, "LockedLiquidity with given id has already been unlocked");
         ll.locked = false;
 
         lockedPremium = lockedPremium.sub(ll.premium);
@@ -148,13 +169,7 @@ contract OddzLiquidityPool is Ownable, IOddzLiquidityPool, ERC20("Oddz USD LP to
         else emit Loss(_id, transferAmount - ll.premium);
     }
 
-    function sendUA(
-        uint256 _id,
-        address payable _account,
-        uint256 _amount
-    ) external onlyOwner {}
-
-    function addLiquidity() private {
+    function addLiquiditySub() private {
         uint256 date = getPresentDayTimestamp();
         daysActiveLiquidity[date] = address(this).balance.add(msg.value);
         // TODO: Check for first time LP
@@ -202,10 +217,9 @@ contract OddzLiquidityPool is Ownable, IOddzLiquidityPool, ERC20("Oddz USD LP to
         activationDate = BokkyPooBahsDateTimeLibrary.timestampFromDate(year, month, day);
     }
 
-    function divCeil(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b > 0, "Invalid Denominator");
-        uint256 c = a / b;
-        if (a % b != 0) c = c + 1;
-        return c;
+    function divisionCeiling(uint256 _numerator, uint256 _denominator) internal pure returns (uint256) {
+        uint256 result = _numerator.div(_denominator, "Invalid Denominator");
+        if (_numerator.mod(_denominator) != 0) result = result + 1;
+        return result;
     }
 }
