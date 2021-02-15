485f24264830d3a33a6d6970e21cedae2ffc210c
bhargav55 <kacharlabhargav21@gmail.com>
2021-02-15 22:34:07 +0530

    test: added testcases for OddzToken

--- test/OddzToken.ts
+++ test/OddzToken.ts
@@ -15,10 +15,18 @@ describe("Unit tests", function () {
   before(async function () {
     this.accounts = {} as Accounts;
     this.signers = {} as Signers;
+    this.delegators = {} as Signers;
+    this.delegatorAccounts = {} as Accounts;
 
     const signers: Signer[] = await ethers.getSigners();
+    
     this.signers.admin = signers[0];
     this.accounts.admin = await signers[0].getAddress();
+    this.signers.admin1 = signers[1];
+    this.accounts.admin1 = await signers[1].getAddress();
+    this.delegators.admin = signers[2];
+    this.delegatorAccounts.admin = await signers[2].getAddress();
+
     this.wallet = wallet;
     this.walletTo = walletTo;
   });
--- test/behaviors/OddzToken.behavior.ts
+++ test/behaviors/OddzToken.behavior.ts
@@ -1,7 +1,11 @@
 import { expect } from "chai";
+import { OddzToken } from "../../typechain";
+import { ecsign } from 'ethereumjs-util';
 //import { bufferToHex, keccakFromString } from "ethereumjs-util";
 
 const TotalSupply = 100000000;
+const Name = "OddzToken";
+const Symbol = "ODDZ";
 export function shouldBehaveLikeOddzToken(): void {
   it("should return the totalSupply", async function () {
     const oddzToken = await this.oddzToken.connect(this.signers.admin);
@@ -22,6 +26,12 @@ export function shouldBehaveLikeOddzToken(): void {
   //   );
   // });
 
+  it("should return the name and symbol", async function () {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    expect(await oddzToken.name()).to.equal(Name);
+    expect(await oddzToken.symbol()).to.equal(Symbol); 
+  });
+
   it("should successfully transfer 1 wei", async function () {
     const oddzToken = await this.oddzToken.connect(this.signers.admin);
     await oddzToken.transfer(this.walletTo.address, 1);
@@ -34,8 +44,187 @@ export function shouldBehaveLikeOddzToken(): void {
     expect(await oddzToken.balanceOf(this.walletTo.address)).to.equal(TotalSupply);
   });
 
+  it("should emit transfer event", async function() {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await expect(oddzToken.transfer(this.walletTo.address, TotalSupply)).to.emit(oddzToken,"Transfer");
+  })
+ 
   it("Cannot transfer above the amount", async function () {
     const oddzToken = await this.oddzToken.connect(this.signers.admin);
     await expect(oddzToken.transfer(this.walletTo.address, TotalSupply + 1)).to.be.reverted;
   });
+
+  it("cannot transfer to 0x0 account", async function() {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await expect(oddzToken.transfer(0x0,100)).to.be.reverted;
+  })
+
+  it("cannot transfer  0 amount", async function() {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await expect(oddzToken.transfer(this.signers.admin1,0)).to.be.reverted;
+
+  })
+
+  it("should approve certain amount", async function(){
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await oddzToken.approve(this.accounts.admin1, 100);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(100);
+
+  })
+
+  it("emit an approval event", async function(){
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await expect(oddzToken.approve(this.accounts.admin1, 100)).to.emit(oddzToken, "Approval");
+
+  })
+
+  it("can set approval amount to zero", async function() {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await oddzToken.approve(this.accounts.admin1, 0);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(0);
+  })
+
+  it("should transfer token by allowed spender", async function() {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await oddzToken.approve(this.accounts.admin1, 100);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(100);
+
+    await oddzToken.connect(this.signers.admin1).transferFrom(this.accounts.admin, this.walletTo.address, 100);
+    expect(await oddzToken.balanceOf(this.walletTo.address)).to.equal(100);
+    expect(await oddzToken.balanceOf(this.accounts.admin)).to.equal(TotalSupply-100);
+  })
+
+  it("should not transfer token more than allowed by spender", async function() {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await oddzToken.approve(this.accounts.admin1, 100);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(100);
+
+    await expect(oddzToken.connect(this.signers.admin1).transferFrom(this.accounts.admin, this.walletTo.address, 101)).to.be.reverted;
+  })
+
+  it("should not be able to transfer tokens again after transferring all the allowances", async function() {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await oddzToken.approve(this.accounts.admin1, 100);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(100);
+    await oddzToken.connect(this.signers.admin1).transferFrom(this.accounts.admin, this.walletTo.address, 100);
+    expect(await oddzToken.balanceOf(this.walletTo.address)).to.equal(100);
+    expect(await oddzToken.balanceOf(this.accounts.admin)).to.equal(TotalSupply-100);
+    await expect(oddzToken.connect(this.signers.admin1).transferFrom(this.accounts.admin, this.walletTo.address, 1)).to.be.reverted;
+    
+  })
+
+  it("should increase allowance", async function() {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await oddzToken.approve(this.accounts.admin1, 100);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(100);
+    await oddzToken.increaseAllowance(this.accounts.admin1,100);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(200);
+    
+  })
+
+  it("should transfer additional tokens after increase allowance", async function() {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await oddzToken.approve(this.accounts.admin1, 100);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(100);
+    await oddzToken.increaseAllowance(this.accounts.admin1,100);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(200);
+    await oddzToken.connect(this.signers.admin1).transferFrom(this.accounts.admin, this.walletTo.address, 200);
+    expect(await oddzToken.balanceOf(this.walletTo.address)).to.equal(200);
+    expect(await oddzToken.balanceOf(this.accounts.admin)).to.equal(TotalSupply-200);
+    
+  })
+
+  it("should decrease allowance", async function() {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await oddzToken.approve(this.accounts.admin1, 100);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(100);
+    await oddzToken.decreaseAllowance(this.accounts.admin1,10);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(90);
+    
+  })
+
+  it("should transfer less tokens after decrease allowance", async function() {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await oddzToken.approve(this.accounts.admin1, 100);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(100);
+    await oddzToken.decreaseAllowance(this.accounts.admin1,10);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(90);
+    await oddzToken.connect(this.signers.admin1).transferFrom(this.accounts.admin, this.walletTo.address, 90);
+   
+    expect(await oddzToken.balanceOf(this.walletTo.address)).to.equal(90);
+    expect(await oddzToken.balanceOf(this.accounts.admin)).to.equal(TotalSupply-90);
+    
+  })
+
+  it("should not transfer more tokens after decreasing allowance", async function() {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await oddzToken.approve(this.accounts.admin1, 100);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(100);
+    await oddzToken.decreaseAllowance(this.accounts.admin1,10);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(90);
+    await expect(oddzToken.connect(this.signers.admin1).transferFrom(this.accounts.admin, this.walletTo.address, 100)).to.be.reverted;
+    
+  });
+
+  it("should be able to set another spender and delegate transfer", async function() {
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    // approve delegator1
+    await oddzToken.approve(this.accounts.admin1, 50);
+    expect(await oddzToken.allowance(this.accounts.admin, this.accounts.admin1)).to.equal(50);
+    // approve delegator2
+    await oddzToken.approve(this.delegatorAccounts.admin,50);
+    expect(await oddzToken.allowance(this.accounts.admin, this.delegatorAccounts.admin)).to.equal(50);
+    // transferFrom initiated by delegator1
+    await oddzToken.connect(this.signers.admin1).transferFrom(this.accounts.admin, this.walletTo.address, 50);
+    expect(await oddzToken.balanceOf(this.walletTo.address)).to.equal(50);
+    expect(await oddzToken.balanceOf(this.accounts.admin)).to.equal(TotalSupply-50);
+    // transfer from initiated bt delegator2
+    await oddzToken.connect(this.delegators.admin).transferFrom(this.accounts.admin, this.walletTo.address, 50);
+    expect(await oddzToken.balanceOf(this.walletTo.address)).to.equal(100);
+    expect(await oddzToken.balanceOf(this.accounts.admin)).to.equal(TotalSupply-50-50);
+    
+  });
+
+
+  it("Should be able to get back accidentally sent tokens", async function () {
+
+    // transfer 100 tokens to user
+    const oddzToken = await this.oddzToken.connect(this.signers.admin);
+    await oddzToken.transfer(this.accounts.admin1, 100);
+    expect(await oddzToken.balanceOf(this.accounts.admin1)).to.equal(100);
+    
+
+    // User transferred 100 tokens to ODDZ contract accidentally
+    await oddzToken.connect(this.signers.admin1).transfer(oddzToken.address, 100);
+    expect(await oddzToken.balanceOf(oddzToken.address)).to.equal(100);
+    expect(await oddzToken.balanceOf(this.accounts.admin1)).to.equal(0);
+    
+
+    // Rescue user funds
+    await expect(oddzToken.connect(this.signers.admin).rescueTokens(
+                                                          oddzToken.address, 
+                                                          this.accounts.admin1, 
+                                                          100)).to.emit(
+                                                          oddzToken,'RescueExcessTokens');
+    expect(await oddzToken.balanceOf(this.accounts.admin1)).to.equal(100);
+    
+
+  });
+
+  // it.only('permit', async function () {
+  //   const oddzToken = await this.oddzToken.connect(this.signers.admin);
+  //   const amount = 100;
+  //   const nonce = await oddzToken.nonces(this.accounts.admin);
+  //   const deadline = 100000000000000; // random timestamp in future
+  //   const hash = await oddzToken.DOMAIN_SEPARATOR();
+    
+    
+  //   console.log("hash: ",hash)
+  //   console.log("getting the sign: ");
+  //   const sig = ecsign(hash, Buffer.from(privateKey,'hex'));
+  //   console.log("permitting")
+  //   await expect(oddzToken.permit(this.accounts.admin, this.accounts.admin1, amount, deadline, sig.v, sig.r, sig.s)).to.emit("Approval")
+       
+    
+  // });
 }
