81a28f14bf7156e50d785e378d4c232ed452ccbb
Krupa Sagar <sagar.krupahb2000@gmail.com>
2021-04-06 09:49:42 +0530

    style: lint fix

--- contracts/Mocks/MockOddzDex.sol
+++ contracts/Mocks/MockOddzDex.sol
@@ -9,7 +9,7 @@ contract MockOddzDex is ISwapUnderlyingAsset {
         address payable _account,
         uint256 _amountIn,
         uint256 _deadline
-    ) public override pure returns (uint256[] memory result) {
+    ) public pure override returns (uint256[] memory result) {
         uint256[] memory result = new uint256[](2);
         uint256 counter = 0;
 
