29653742f4b9c8665aafa9a30c086fe52073737d
bhargav55 <kacharlabhargav21@gmail.com>
2021-04-13 16:51:19 +0530

    test: case with less slippage and more slippage

--- test/behaviors/OddzOptionManager.behavior.ts
+++ test/behaviors/OddzOptionManager.behavior.ts
@@ -524,6 +524,7 @@ export function shouldBehaveLikeOddzOptionManager(): void {
       this.usdcToken,
       this.ethToken,
     );
+    await provider.send("evm_snapshot", []);
     await addLiquidity(this.oddzLiquidityPool, this.signers.admin, 1000000);
     await getPremiumWithSlippageAndBuy(
       this.oddzOptionManager,
@@ -580,6 +581,7 @@ export function shouldBehaveLikeOddzOptionManager(): void {
     );
     await expect((await oddzLiquidityPool.lpPremium(this.accounts.admin)).toNumber()).to.equal(0);
 
+    await provider.send("evm_revert", [utils.hexStripZeros(utils.hexlify(++snapshotCount))]);
     await provider.send("evm_revert", [utils.hexStripZeros(utils.hexlify(++snapshotCount))]);
     await provider.send("evm_revert", [utils.hexStripZeros(utils.hexlify(++snapshotCount))]);
     await provider.send("evm_revert", [utils.hexStripZeros(utils.hexlify(++snapshotCount))]);
@@ -1409,5 +1411,67 @@ export function shouldBehaveLikeOddzOptionManager(): void {
         OptionType.Call,
       ),
     ).to.be.revertedWith("Premium crossed slippage tolerance");
+  });
+
+  it("should revert  with less slippage and buy for more slippage with same iv", async function () {
+    const oddzOptionManager = await this.oddzOptionManager.connect(this.signers.admin);
+    const oddzVolatility = await this.oddzVolatility.connect(this.signers.admin);
+
+    const pairId = await getAssetPair(
+      this.oddzAssetManager,
+      this.signers.admin,
+      this.oddzPriceOracleManager,
+      this.oddzPriceOracle,
+      this.usdcToken,
+      this.ethToken,
+    );
+    await addLiquidity(this.oddzLiquidityPool, this.signers.admin, 1000000);
+    const premiumWithSlippage = await getPremiumWithSlippageAndBuy(
+      this.oddzOptionManager,
+      pairId,
+      utils.formatBytes32String("B_S"),
+      getExpiry(1),
+      BigNumber.from(utils.parseEther("1")), // number of options
+      BigNumber.from(170000000000),
+      OptionType.Call,
+      3,
+      false,
+    );
+    await oddzVolatility.setIv(182831, 5);
+    await expect(
+      oddzOptionManager.buy(
+        pairId,
+        utils.formatBytes32String("B_S"),
+        BigInt(premiumWithSlippage),
+        getExpiry(1),
+        BigNumber.from(utils.parseEther("1")), // number of options
+        BigNumber.from(170000000000),
+        OptionType.Call,
+      ),
+    ).to.be.revertedWith("Premium crossed slippage tolerance");
+
+    const premiumWithSlippage1 = await getPremiumWithSlippageAndBuy(
+      this.oddzOptionManager,
+      pairId,
+      utils.formatBytes32String("B_S"),
+      getExpiry(1),
+      BigNumber.from(utils.parseEther("1")), // number of options
+      BigNumber.from(170000000000),
+      OptionType.Call,
+      3.1,
+      false,
+    );
+    await oddzVolatility.setIv(182831, 5);
+    await expect(
+      oddzOptionManager.buy(
+        pairId,
+        utils.formatBytes32String("B_S"),
+        BigInt(premiumWithSlippage1),
+        getExpiry(1),
+        BigNumber.from(utils.parseEther("1")), // number of options
+        BigNumber.from(170000000000),
+        OptionType.Call,
+      ),
+    ).to.emit(oddzOptionManager, "Buy");
   });
 }
