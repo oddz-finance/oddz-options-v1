263dd27c305cee40c752db17d278b21863c67199
manti <manti.rvce@gmail.com>
2021-05-10 20:20:06 +0530

    fix: added more delay to handle current out of sync(24 hour)

--- contracts/Integrations/VolatilityOracle/Chainlink/ChainlinkIVOracle.sol
+++ contracts/Integrations/VolatilityOracle/Chainlink/ChainlinkIVOracle.sol
@@ -9,7 +9,7 @@ contract ChainlinkIVOracle is AccessControl, IOddzVolatilityOracle {
     using Address for address;
 
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
-    uint256 public delayInSeconds = 30000 * 60;
+    uint256 public delayInSeconds = 90000 * 60;
     mapping(uint8 => bool) public allowedPeriods;
     mapping(uint256 => uint8) public ivPeriodMap;
 
