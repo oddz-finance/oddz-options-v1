62d092a81562dcd980e13403d586f0c4783af375
Krupa Sagar <sagar.krupahb2000@gmail.com>
2021-05-20 17:34:07 +0530

    feat: oddz oracle

--- contracts/Integrations/VolatilityOracle/Oddz/OddzVolatility.sol
+++ contracts/Integrations/VolatilityOracle/Oddz/OddzVolatility.sol
@@ -5,41 +5,31 @@ import "../../../Oracle/IOddzVolatilityOracle.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract OddzVolatility is Ownable, IOddzVolatilityOracle {
-    struct AssetVolatility {
-        bytes32 asset;
-        bytes32 strike;
-        uint8 ivDecimal;
-        uint256 iv;
-        uint256 timestamp;
-    }
+    uint256 public delayInSeconds = 30 * 60;
+    uint256 public lastUpdatedAt;
+    mapping(uint8 => bool) public allowedPeriods;
+    mapping(uint256 => uint8) public ivPeriodMap;
 
-    mapping(bytes32 => mapping(bytes32 => AssetVolatility)) internal assetVolatilityMap;
+    mapping(bytes32 => address) public addressMap;
 
-    uint256 public MIN_IV = 10000;
-    uint256 public MAX_IV = 1e8;
-    uint256 public MIN_DECIMAL = uint256(5);
-    uint256 public MAX_DECIMAL = uint256(8);
-
-    modifier validIv(uint256 _iv) {
-        require(_iv >= MIN_IV && _iv <= MAX_IV, "Invalid IV");
-        _;
-    }
-
-    modifier validDecimal(uint256 _decimal) {
-        require(_decimal >= MIN_DECIMAL && _decimal <= MAX_DECIMAL, "Invalid Decimal");
-        _;
-    }
+    //bytes(underlying,strike,expiry) => volPerc => val
+    mapping(bytes32 => mapping(uint8 => uint256)) public volatility;
+    uint256 public volatilityPrecision = 2;
 
     function getIv(
-        bytes32 _asset,
+        bytes32 _underlying,
         bytes32 _strike,
         uint256 _expiration,
         uint256 _currentPrice,
         uint256 _strikePrice
-    ) external view override returns (uint256 iv, uint8 decimals) {
-        AssetVolatility storage av = assetVolatilityMap[_asset][_strike];
-        iv = av.iv;
-        decimals = av.ivDecimal;
+    ) public view override returns (uint256 iv, uint8 decimals) {
+        require((lastUpdatedAt + delayInSeconds) <= block.timestamp, "Oddz IV: out of sync");
+        // update _expiration to next day if spillover seconds in a day
+        if ((_expiration % 1 days) > 0) _expiration = _expiration + 1 days;
+        uint256 _expirationDay = (_expiration / 1 days);
+        iv = _getIv(_underlying, _strike, _expirationDay, _currentPrice, _strikePrice, iv, decimals);
+        // converting iv from percentage to value
+        iv = iv / 100;
     }
 
     function setPairContract(
@@ -49,12 +39,91 @@ contract OddzVolatility is Ownable, IOddzVolatilityOracle {
         uint8 _aggregatorPeriod
     ) public override {}
 
-    function setIv(
-        bytes32 _asset,
+    function setDelay(uint256 _delay) public onlyOwner {
+        delayInSeconds = _delay;
+    }
+
+    /**
+     * @notice Function to set iv precision decimals
+     * @param _precision volatility precision
+     */
+    function setVolatilityPrecision(uint8 _precision) public onlyOwner {
+        volatilityPrecision = _precision;
+    }
+
+    /**
+     * @notice Function to get valid percentage difference in strike and current price
+     * @param _perc Calculated percentage difference
+     * @param _isNeg if the percentage is negative
+     * @return _volPercentage valid percentage difference (90, 40, 20, 10)
+     */
+    function _getVolPercentage(uint256 _perc, bool _isNeg) private pure returns (uint8 _volPercentage) {
+        if (_isNeg) _volPercentage = _getNegPercentage(_perc);
+        else _volPercentage = _getPosPercentage(_perc);
+    }
+
+    /**
+     * @notice Function to get valid negative percentage difference in strike and current price
+     * @param _perc Calculated negative percentage difference
+     * @return _volPercentage valid negative percentage difference (90, 40, 20, 10)
+     */
+    function _getNegPercentage(uint256 _perc) private pure returns (uint8 _volPercentage) {
+        if (_perc > 0 && _perc <= 5) _volPercentage = 5;
+        else if (_perc > 5 && _perc <= 10) _volPercentage = 10;
+        else if (_perc > 10 && _perc <= 20) _volPercentage = 20;
+        else if (_perc > 20 && _perc <= 40) _volPercentage = 40;
+        else if (_perc > 40) _volPercentage = 90;
+    }
+
+    /**
+     * @notice Function to get valid posituve percentage difference in strike and current price
+     * @param _perc Calculated positive percentage difference
+     * @return _volPercentage valid positive percentage difference (90, 40, 20, 10)
+     */
+    function _getPosPercentage(uint256 _perc) private pure returns (uint8 _volPercentage) {
+        if (_perc > 0 && _perc <= 5) _volPercentage = 105;
+        else if (_perc > 5 && _perc <= 10) _volPercentage = 110;
+        else if (_perc > 10 && _perc <= 20) _volPercentage = 120;
+        else if (_perc > 20 && _perc <= 40) _volPercentage = 140;
+        else if (_perc > 40) _volPercentage = 190;
+    }
+
+    /**
+     * @notice Function to add volatility for an option based on volatility percentage
+     * @param _underlying Underlying asset name
+     * @param _strike Strike aseet name
+     * @param _expiration expiration of option
+     * @param _volPercentage Volatility percentage difference (90, 40, 20, 10)
+     * @param _volatility Volatility value
+     */
+    function addVolatilityMapping(
+        bytes32 _underlying,
         bytes32 _strike,
-        uint256 _iv,
-        uint8 _decimal
-    ) external onlyOwner validIv(_iv) validDecimal(_decimal) {
-        assetVolatilityMap[_asset][_strike] = AssetVolatility(_asset, _strike, _decimal, _iv, block.timestamp);
+        uint256 _expiration,
+        uint8 _volPercentage,
+        uint256 _volatility // 96.68 => 9668
+    ) public onlyOwner {
+        volatility[keccak256(abi.encode(_underlying, _strike, _expiration))][_volPercentage] = _volatility;
+        lastUpdatedAt = block.timestamp;
+    }
+
+    function _getIv(
+        bytes32 _underlying,
+        bytes32 _strike,
+        uint256 _expiration,
+        uint256 _currentPrice,
+        uint256 _strikePrice,
+        uint256 _chainlinkVol,
+        uint256 _ivDecimal
+    ) private view returns (uint256) {
+        if (_currentPrice == _strikePrice) return _chainlinkVol;
+        uint8 volPercentage;
+        if (_strikePrice > _currentPrice)
+            volPercentage = _getVolPercentage(((_strikePrice - _currentPrice) * 100) / _currentPrice, false);
+        else if (_strikePrice < _currentPrice)
+            volPercentage = _getVolPercentage(((_currentPrice - _strikePrice) * 100) / _strikePrice, true);
+        return
+            (volatility[keccak256(abi.encode(_underlying, _strike, _expiration))][volPercentage] * (10**_ivDecimal)) /
+            (10**volatilityPrecision);
     }
 }
