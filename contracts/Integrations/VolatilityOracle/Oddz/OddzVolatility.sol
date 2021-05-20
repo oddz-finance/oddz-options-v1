dbaed5e8bf806b8e9697688e8659b2578a8a8cab
Krupa Sagar <sagar.krupahb2000@gmail.com>
2021-05-20 19:28:41 +0530

    feat: update oddz iv to use strike and current price

--- contracts/Integrations/VolatilityOracle/Oddz/OddzVolatility.sol
+++ contracts/Integrations/VolatilityOracle/Oddz/OddzVolatility.sol
@@ -7,14 +7,80 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract OddzVolatility is Ownable, IOddzVolatilityOracle {
     uint256 public delayInSeconds = 30 * 60;
     uint256 public lastUpdatedAt;
-    mapping(uint8 => bool) public allowedPeriods;
-    mapping(uint256 => uint8) public ivPeriodMap;
+    mapping(uint256 => bool) public allowedPeriods;
+    mapping(uint256 => uint256) public ivPeriodMap;
 
     mapping(bytes32 => address) public addressMap;
 
     //bytes(underlying,strike,expiry) => volPerc => val
     mapping(bytes32 => mapping(uint8 => uint256)) public volatility;
-    uint256 public volatilityPrecision = 2;
+    mapping(bytes32 => uint256) public defaultIvMap;
+    uint8 public volatilityPrecision = 2;
+
+    modifier allowedPeriod(uint256 _aggregatorPeriod) {
+        require(allowedPeriods[_aggregatorPeriod] == true, "Chainlink IV: Invalid aggregator period");
+        _;
+    }
+
+    constructor() {
+        // Add default IV aggregation periods
+        addAllowedPeriods(1);
+        addAllowedPeriods(2);
+        addAllowedPeriods(7);
+        addAllowedPeriods(14);
+        addAllowedPeriods(21);
+        addAllowedPeriods(28);
+
+        // Add default pair map for Day with IV Agg period
+        mapDaysToIVPeriod(1, 1);
+        mapDaysToIVPeriod(2, 2);
+        mapDaysToIVPeriod(3, 2);
+        mapDaysToIVPeriod(4, 2);
+        mapDaysToIVPeriod(5, 7);
+        mapDaysToIVPeriod(6, 7);
+        mapDaysToIVPeriod(7, 7);
+        mapDaysToIVPeriod(8, 7);
+        mapDaysToIVPeriod(9, 7);
+        mapDaysToIVPeriod(10, 7);
+        mapDaysToIVPeriod(11, 14);
+        mapDaysToIVPeriod(12, 14);
+        mapDaysToIVPeriod(13, 14);
+        mapDaysToIVPeriod(14, 14);
+        mapDaysToIVPeriod(15, 14);
+        mapDaysToIVPeriod(16, 14);
+        mapDaysToIVPeriod(17, 14);
+        mapDaysToIVPeriod(18, 21);
+        mapDaysToIVPeriod(19, 21);
+        mapDaysToIVPeriod(20, 21);
+        mapDaysToIVPeriod(21, 21);
+        mapDaysToIVPeriod(22, 21);
+        mapDaysToIVPeriod(23, 21);
+        mapDaysToIVPeriod(24, 21);
+        mapDaysToIVPeriod(25, 28);
+        mapDaysToIVPeriod(26, 28);
+        mapDaysToIVPeriod(27, 28);
+        mapDaysToIVPeriod(28, 28);
+        mapDaysToIVPeriod(29, 28);
+        mapDaysToIVPeriod(30, 28);
+        mapDaysToIVPeriod(31, 28);
+    }
+
+    /**
+     @notice Add/update allowed Option expiry preiod to IV period map
+     @param _day option expiry days
+     @param _ivAgg IV aggregation period
+     */
+    function mapDaysToIVPeriod(uint256 _day, uint8 _ivAgg) public onlyOwner allowedPeriod(_ivAgg) {
+        ivPeriodMap[_day] = _ivAgg;
+    }
+
+    /**
+     @notice Add/update allowed IV period map
+     @param _ivAgg IV aggregation period
+     */
+    function addAllowedPeriods(uint8 _ivAgg) public onlyOwner {
+        allowedPeriods[_ivAgg] = true;
+    }
 
     function getIv(
         bytes32 _underlying,
@@ -23,13 +89,13 @@ contract OddzVolatility is Ownable, IOddzVolatilityOracle {
         uint256 _currentPrice,
         uint256 _strikePrice
     ) public view override returns (uint256 iv, uint8 decimals) {
-        require((lastUpdatedAt + delayInSeconds) <= block.timestamp, "Oddz IV: out of sync");
+        require((lastUpdatedAt + delayInSeconds) > block.timestamp, "Oddz IV: out of sync");
         // update _expiration to next day if spillover seconds in a day
         if ((_expiration % 1 days) > 0) _expiration = _expiration + 1 days;
-        uint256 _expirationDay = (_expiration / 1 days);
-        iv = _getIv(_underlying, _strike, _expirationDay, _currentPrice, _strikePrice, iv, decimals);
-        // converting iv from percentage to value
-        iv = iv / 100;
+        iv = _getIv(_underlying, _strike, ivPeriodMap[_expiration / 1 days], _currentPrice, _strikePrice);
+        if (iv == 0) iv = defaultIvMap[keccak256(abi.encode(_underlying, _strike))];
+        require(iv > 0, "Oddz IV: invalid");
+        decimals = volatilityPrecision;
     }
 
     function setPairContract(
@@ -92,7 +158,7 @@ contract OddzVolatility is Ownable, IOddzVolatilityOracle {
      * @notice Function to add volatility for an option based on volatility percentage
      * @param _underlying Underlying asset name
      * @param _strike Strike aseet name
-     * @param _expiration expiration of option
+     * @param _expiration expiration period in days
      * @param _volPercentage Volatility percentage difference (90, 40, 20, 10)
      * @param _volatility Volatility value
      */
@@ -102,9 +168,24 @@ contract OddzVolatility is Ownable, IOddzVolatilityOracle {
         uint256 _expiration,
         uint8 _volPercentage,
         uint256 _volatility // 96.68 => 9668
-    ) public onlyOwner {
+    ) public onlyOwner allowedPeriod(_expiration) {
         volatility[keccak256(abi.encode(_underlying, _strike, _expiration))][_volPercentage] = _volatility;
         lastUpdatedAt = block.timestamp;
+    }
+
+    /**
+     * @notice Function to set default volatility for an asset pair
+     * @param _underlying Underlying asset name
+     * @param _strike Strike aseet name
+     * @param _volatility Volatility value
+     * Note: IV decimal is defaulted to volatilityPrecision
+     */
+    function setDefaultIv(
+        bytes32 _underlying,
+        bytes32 _strike,
+        uint256 _volatility // 96.68 => 9668
+    ) external onlyOwner {
+        defaultIvMap[keccak256(abi.encode(_underlying, _strike))] = _volatility;
     }
 
     function _getIv(
@@ -112,18 +193,13 @@ contract OddzVolatility is Ownable, IOddzVolatilityOracle {
         bytes32 _strike,
         uint256 _expiration,
         uint256 _currentPrice,
-        uint256 _strikePrice,
-        uint256 _chainlinkVol,
-        uint256 _ivDecimal
+        uint256 _strikePrice
     ) private view returns (uint256) {
-        if (_currentPrice == _strikePrice) return _chainlinkVol;
         uint8 volPercentage;
-        if (_strikePrice > _currentPrice)
+        if (_strikePrice >= _currentPrice)
             volPercentage = _getVolPercentage(((_strikePrice - _currentPrice) * 100) / _currentPrice, false);
-        else if (_strikePrice < _currentPrice)
-            volPercentage = _getVolPercentage(((_currentPrice - _strikePrice) * 100) / _strikePrice, true);
-        return
-            (volatility[keccak256(abi.encode(_underlying, _strike, _expiration))][volPercentage] * (10**_ivDecimal)) /
-            (10**volatilityPrecision);
+        else volPercentage = _getVolPercentage(((_currentPrice - _strikePrice) * 100) / _strikePrice, true);
+
+        return volatility[keccak256(abi.encode(_underlying, _strike, _expiration))][volPercentage];
     }
 }
