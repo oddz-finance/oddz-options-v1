bb8a567fb68754c91a08bee05c29b770fd026490
bhargav55 <kacharlabhargav21@gmail.com>
2021-07-12 15:00:44 +0530

    fix: check with slippage with oracle price

--- contracts/Integrations/Dex/PancakeSwap/PancakeSwapForUnderlyingAsset.sol
+++ contracts/Integrations/Dex/PancakeSwap/PancakeSwapForUnderlyingAsset.sol
@@ -6,14 +6,17 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IPancakeSwap.sol";
 import "../../../Swap/ISwapUnderlyingAsset.sol";
+import "../Oracle/IOddzPriceOracleManager.sol";
 
 contract PancakeSwapForUnderlyingAsset is Ownable, ISwapUnderlyingAsset {
     using SafeERC20 for ERC20;
 
     IPancakeSwap public pancakeSwap;
+    IOddzPriceOracleManager public oracle;
 
-    constructor(address _router) {
+    constructor(address _router, IOddzPriceOracleManager _oracle) {
         pancakeSwap = IPancakeSwap(_router);
+        oracle = _oracle;
     }
 
     /**
@@ -38,10 +41,14 @@ contract PancakeSwapForUnderlyingAsset is Ownable, ISwapUnderlyingAsset {
         path[0] = _fromToken;
         path[1] = _toToken;
         ERC20(_fromToken).safeApprove(address(pancakeSwap), _amountIn);
-        // gets amount of output tokens for input tokens
-        uint256[] memory amounts = pancakeSwap.getAmountsOut(_amountIn, path);
-        // /10000 --> slippage% decimals restricted to 2 (2.55)
-        uint256 amountOutMin = amounts[amounts.length - 1] - (amounts[amounts.length - 1] * _slippage) / 10000;
+
+        (uint256 cp, uint8 decimal) =
+            oracle.getUnderlyingPrice(
+                assets[_toToken],
+                0x5553440000000000000000000000000000000000000000000000000000000000
+            );
+        uint256 swapAmount = _amountIn / (cp / 10**decimal);
+        uint256 amountOutMin = swapAmount - (swapAmount * _slippage) / 10000;
         result = pancakeSwap.swapExactTokensForTokens(_amountIn, amountOutMin, path, address(this), _deadline);
         // converting address to address payable
         ERC20(address(uint160(_toToken))).safeTransfer(_account, result[1]);
